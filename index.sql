6e049fbed7e83d12a2d9435681892e49d92d6ff7
Victor Perin <victor_perin@nerdhitech.com.br>
2015-02-24 22:14:22 -0300

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,22 @@
+create table Fornecedores (
+
+	CodForn integer,
+	NomeForn varchar(50),
+	EndForn varchar(50),
+	PRIMARY key(CodForn)
+
+);
+
+create table Pecas (
+	CodPe integer,
+	NomePe varchar(50),
+	Cor ENUM ('Azul', 'Vermelho', 'Verde','Amarelo', 'Branco'),
+	PRIMARY key(CodPe)
+);
+
+create table Catalogo (
+	CodForn integer,
+	CodPe integer,
+	Preco money,
+
+}
